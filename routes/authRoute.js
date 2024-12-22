5634f3c2e8109e43b2cdde909b89b18d1a407a54
Swarnab <swarnabmessibanerjee@gmail.com>
2024-12-22 11:21:23 +0530

    backend pushed

---
+++ routes/authRoute.js
@@ -0,0 +1,32 @@
+import express from "express";
+import {
+  forgotPassword,
+  getProfile,
+  login,
+  logout,
+  register,
+  resetPassword,
+  verifyCode,
+} from "../controllers/userController.js";
+import { checkisAuth } from "../middlewares/checkAuth.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+const router = express.Router();
+
+router.post("/register", register);
+
+router.post("/verify", verifyCode);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+router.post("/forgot-password", forgotPassword);
+
+router.post("/reset-password/:token", resetPassword);
+
+router.get("/profile", checkisAuth, getProfile);
+
+export default router;
